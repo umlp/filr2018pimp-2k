57a7c7d304453d91b44a150a641d3105537c3df4
lulmer <36041351+lulmer@users.noreply.github.com>
2018-05-23 13:58:57 +0200

    Update and rename style.css.scss to style.php

--- resources/views/layouts/css/style.php
+++ resources/views/layouts/css/style.php
@@ -1,3 +1,12 @@
+<?php
+/*** set the content type header ***/
+/*** Without this header, it wont work ***/
+header("Content-type: text/css");
+$font_family = 'Arial, Helvetica, sans-serif';
+$font_size = '0.7em';
+$border = '1px solid';
+?>
+
 ul.sdt_menu{
 	margin:0;
 	margin-top:70px;
